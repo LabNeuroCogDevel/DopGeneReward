7dd89b750c5bc7961f5a652c62b13ff429ec281a
Will Foran <willforan@gmail.com>
2013-11-25 17:07:18 -0500

    update names ledgend and aesthetic, fix plots (working subset)

--- plot.R
+++ plot.R
@@ -20,29 +20,15 @@ for(gene in c('MAOA','CompositeRecoded','COMT')) { #'DAT1',
  table <- read.table( paste('data/',gene,'_AgeInverseCentered_RestingState.csv',sep=""),   sep=",", header=1, row.names=1)
 
  # set names
-  levels(table$Subcortical)<-c("L_NAcc",   "L_caudate" ,"L_putamen", "R_NAcc",   "R_caudate", "R_putamen")
-  levels(table$PFC)<-c("dACC","L_IFG", "L_LateralOFC",
-                       "L_MFG", "L_SFG", "mOFC",
-                       "R_IFG", "R_LateralOFC", "R_MFG",
-                       "R_SFG", "vACC" )
+ # levels(table$Subcortical)
+ #[1] "Accum"   "Caudate" "Putamen"
+ levels(table$Subcortical)<-c("Nucleus Accumbens", "Caudate", "Putanmen" )
 
- reorderPFC<- c("L_IFG","R_IFG",
-               "L_LateralOFC","R_LateralOFC",
-               "L_MFG","R_MFG",
-               "L_SFG", "R_SFG",
-               "dACC","vACC",
-               "mOFC" )
-
- reorderSC <- c(
-             "L_NAcc", "R_NAcc",
-             "L_caudate", "R_caudate",
-             "L_putamen", "R_putamen"
-           )
-
- # reorder PFC graphs
- #table$PFC = factor(table$PFC,levels(table$PFC)[c(11,10,9,8,1:7)])
- table$PFC = factor(table$PFC,reorderPFC)
- table$Subcortical = factor(table$Subcortical,reorderSC)
+ # > levels(table$PFC)
+ # [1] "Dorsal_ACC"           "InferiorFrontalGyrus" "Lateral_OFC"          "Medial_OFC"           "MiddleFrontalGyrus" 
+ # [6] "SuperiorFrontalGyrus" "Ventral_ACC" 
+ levels(table$PFC)<-c("dACC","IFG", "Lateral OFC",
+                      "Medial OFC", "MFG", "SFC", "vACC")
 
  # how does the table call this gene (ie. append .Genotype)
  genename <- paste( gene, '.Genotype' , sep="")
@@ -50,19 +36,32 @@ for(gene in c('MAOA','CompositeRecoded','COMT')) { #'DAT1',
  # set genotype as a factor
  table[,genename] <- as.factor(table[,genename])
  names(table)[3] <- gene
+ # this gets a bit hairy
+ # "gene" at the for loop level is "COMT", "DAT1", etc
+ # the "gene" as dataframe column name (eg table$COMT) is the gene type (e.g. Va/Met)
+ if(gene == 'COMT' ) {
+   levels(table[,gene])<-c("Val/Val" ,"Val/Met", "Met/Met")
+ } else if(gene == 'MOAO'){
+   levels(table[,gene])<-c("4R/4R" ,"3R/4R", "3R/3R")
+ } else if(gene == 'DAT1'){
+   levels(table[,gene])<-c("10R/10R" ,"9R")
+ }else{ #COMPOSITE
+   levels(table[,gene])<-c("0.5" ,"1.0","1.5","2.0","2.5","3.0")
+ }
+
  #levels(table[,genename]) <- c(1,2,3) # set the level names to something useful
 
  # plot
  p <- ggplot(data=table,
        aes_string(x="1/(AgeInverseCentered+1/15.36)",y="reducedCorrPercentileContiguous", group=gene,color=gene)
-      )
+     ) + theme_bw()
  p <- p + ggtitle(gene) +
   geom_line()+                          # add a line
   facet_grid(PFC ~  Subcortical)+       # break up by PFC+Subcortical comparison
   scale_color_manual(values=color)+     # set the desired colors
   theme(text=element_text(size=8))+     # reduce font size everywhere to 8
   scale_y_continuous("Correlation\n(Fisher's z)")+ # y axis title
-  scale_x_continuous('Age',            # x axis title
+  scale_x_continuous('Age (Years)',            # x axis title
                      breaks=c(10,15,20))  # only show 3 numbers on the x axis
 
  # put plot into pdf
@@ -76,13 +75,13 @@ for(gene in c('MAOA','CompositeRecoded','COMT')) { #'DAT1',
  table$PFCSubCombo <- sprintf("%s - %s",table$PFC,table$Subcortical)
  for (PFCSubCombo in unique(table$PFCSubCombo)) {
    cat("\t",PFCSubCombo,"\n")
-   subtable <- subset(table,PFCSubCombo==PFCSubCombo)
+   subtable <- table[table$PFCSubCombo==PFCSubCombo,]
 
-   p <- ggplot(data=subtable,
+   pbase <- ggplot(data=subtable,
          aes_string(x="1/(AgeInverseCentered+1/15.36)",y="reducedCorrPercentileContiguous", group=gene,color=gene)
-        )
+        ) + theme_bw()
    # single plot for each
-   p <- p + ggtitle(sprintf("%s: %s",gene, PFCSubCombo)) +
+   p <- pbase + ggtitle(sprintf("%s: %s",gene, PFCSubCombo)) +
     geom_line()+                          # add a line
     scale_color_manual(values=color)+     # set the desired colors
     theme(text=element_text(size=8))+     # reduce font size everywhere to 8
@@ -91,7 +90,7 @@ for(gene in c('MAOA','CompositeRecoded','COMT')) { #'DAT1',
                        breaks=c(10,15,20))  # only show 3 numbers on the x axis
    imgsavedir <- file.path('imgs','idv',gene);
    dir.create(imgsavedir,recursive=T)
-   pdf(paste(imgsavedir,PFCSubCombo,'.pdf',sep=""),width=11,height=8)#, width=600,height=400)
+   pdf(paste(imgsavedir,'/',PFCSubCombo,'.pdf',sep=""),width=11,height=8)#, width=600,height=400)
    print(p)
    dev.off()
  }
